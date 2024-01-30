c10a4ccb1e81cd5384c612fa95e7a4e4dbeba517
bakeaid <138861306+bakeaid@users.noreply.github.com>
2024-01-30 02:41:11 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import QuizMap from './components/quizmap';
+import NewMap from './components/newmap';
 
 function App() {
+  const [pokazQuizMap, setPokazQuizMap] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="MAP-QUIZ">
+        <label>
+          <input
+            type="checkbox"
+            checked={pokazQuizMap}
+            onChange={() => setPokazQuizMap(!pokazQuizMap)}
+          />
+          Pokaż Wszystkie
+        </label>
       </header>
+      {pokazQuizMap ? <NewMap /> : <QuizMap />}
     </div>
   );
 }
